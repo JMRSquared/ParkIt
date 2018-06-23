b6a55a986a7d090a3a8b57a8cfafc44a2b80c37d
Sirwali Joseph <mulavhe@gmail.com>
2018-06-23 13:52:50 +0200

    Fixed the icons

--- src/components/OpenGate.vue
+++ src/components/OpenGate.vue
@@ -1,9 +1,11 @@
 <template>
   <Page class="page">
-    <ActionBar class="action-bar" title="Open Gate"/>
-
+    <ActionBar class="action-bar" title="Open Gate" />
+  
     <StackLayout>
       <Label text="Open Gate"></Label>
+      <label verticalAlignment="center" textAlignment="center" class="mdi m-10" fontSize="25%" :text="'mdi-phone' | fonticon"></label>
+  
     </StackLayout>
   </Page>
 </template>
--- src/components/Parked.vue
+++ src/components/Parked.vue
@@ -1,9 +1,13 @@
 <template>
   <Page class="page">
-    <ActionBar class="action-bar" title="You are now parked"/>
-
+    <ActionBar class="action-bar" title="You are now parked" />
+  
     <StackLayout>
-      <Label text="Parked"></Label>
+      <GridLayout rows="*" columns="*">
+        <CardView row="0" col="0" verticalAlignment="center" padding="10" margin="4" elevation="10" shadowOffsetHeight="10" shadowOpacity="0.2" shadowRadius="50">
+          <Label text="Parked"></Label>
+        </CardView>
+      </GridLayout>
     </StackLayout>
   </Page>
 </template>
